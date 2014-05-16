d26ae79b4944a269fc2eaa145e1d025bd7625ba2
yoanngern <yoann@yoanngern.ch>
2014-05-16 15:36:54 +0200

    start page

---
+++ start/js/_resize.js
@@ -0,0 +1,63 @@
+$(document).ready( function() {
+	//resize();
+	
+	timer = setInterval(function() {
+        resize();
+        clearInterval(timer);
+    }, 100);
+    
+	// If the window is resised
+	$(window).resize(function() {
+        resize();
+	});
+});
+
+function resize() {
+    var windowWidth = $(window).width();
+    var windowHeight = $(window).height();
+    
+    $(".player").each( function() {
+        var playerWidth = $(this).width();
+        $(this).css("height", playerWidth*9/16);
+        $("iframe", this).attr("width", playerWidth);
+        $("iframe", this).attr("height", playerWidth*9/16);
+    });
+    
+    $(".content .right").each( function() {
+        var right = $(this);
+        var content = $(this).parent();
+        var left = $(".left", content);
+        
+        var leftHeight = $(left).height();
+        var rightHeight = $(right).height();
+        
+        if(rightHeight > leftHeight) {
+            $(right).addClass("bigger");
+            $(left).addClass("smaller");
+        } else {
+            $(right).removeClass("bigger");
+            $(left).removeClass("smaller");
+        }
+        
+    });
+    
+    
+    $("article.video").each( function() {
+        var videoPos = $(".player", this).position();
+        var videoHeight = $(".player", this).height();
+        var videoWidth = $(".player", this).width();
+        
+        $("#left, #right", this).css("height", videoHeight);
+        
+        var arrowHeight = $("#left", this).height();
+        
+        var arrowTop = videoPos.top + videoHeight/2 - arrowHeight/2;
+        var arrowWidth = (windowWidth - videoWidth) /2;
+        
+        $("#left", this).css("left", 0);
+        $("#right", this).css("right", 0);
+        $("#left, #right", this).css("top", arrowTop);
+        $("#left, #right", this).css("width", arrowWidth);
+        
+    });
+}
