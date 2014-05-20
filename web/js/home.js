af8e83ed80ec3b9b18d92105311f5c93d2106876
fredmontet <hello@fredmontet.com>
2014-05-20 08:40:06 +0200

    La map de malade

--- web/js/home.js
+++ web/js/home.js
@@ -1,24 +1,64 @@
 $(document).ready(function(){
+	
 	init();
 
+});//$(document).ready END
 
-});
 
 $(window).load(function(){
+
 	menu();
 
-//Map
-/************************************************************/
 
-var panZoomTiger = svgPanZoom('#map')
-console.log(panZoomTiger.isZoomEnabled());
-panZoomTiger.zoomAtPointBy(7, {x: 950, y: 450});
-console.log(panZoomTiger.getZoom());
+	//Initialisation et management de l'affichage de la map
+	/************************************************************/
+
+	//A titre informatif
+	var screenWidth = $(window).width();
+	var screenHeight = $(window).height();
+	console.log("width = "+screenWidth+"  height = "+screenHeight);
+
+	//Initialisation de la carte
+	var panZoomTiger = svgPanZoom('#map')
+
+	//Variables de positionnement de la carte
+	var svgPosX = Math.floor(($(window).width()/100)*40);
+	var svgPosY = Math.floor(($(window).height()/100)*55);
+
+	//Paramètres d'affichage de la carte
+	panZoomTiger.setMaxZoom(17);
+	panZoomTiger.setMinZoom(1);
+	panZoomTiger.zoomAtPointBy(4, {x: svgPosX, y: svgPosY});
 
 
+	//Interactions avec la map
+	/************************************************************/
+
+	//Préparation pour pouvoir appeler les éléments de la carte dans
+	//le DOM SVG en jQuery sans confusions.
+	//var map = document.getElementById("map");
+	//console.log(map);
+
+	//hover sur un projet
+	$(".project").mouseover(function(){
+		var initialRadius = $(this).attr("r");
+		console.log(initialRadius);
+		$(this).attr("r","20");
+		return initialRadius;
+	});
+
+	$(".project").mouseleave(function(initialRadius){
+		console.log(initialRadius);
+		$(this).attr("r",initialRadius);
+	});
 
 	
-});
+
+
+
+
+});//$(window).load END
+
 
 
 //Functions
@@ -30,7 +70,7 @@ function init(){
 
 function menu(){
 	$("#toggleMenu").click(function(){
-		$("#menu").slideToggle('slow');
+		$("#menu").slideToggle();
 	});
 }
 
