b2c8f9198445c457865e5272f40588e230c97d80
fredmontet <hello@fredmontet.com>
2014-05-20 16:42:00 +0200

    interaction map

--- web/js/home.js
+++ web/js/home.js
@@ -1,9 +1,44 @@
 // @codekit-prepend "vendor/jquery-2.1.1.js"
 // @codekit-prepend "vendor/svg-pan-zoom.js"
+// @codekit-prepend "vendor/snap.svg.js"
 
 $(document).ready(function(){
 	
 	init();
+
+	//Interactions avec la map
+	/************************************************************/
+	
+	//Initialisation des action pour les différents projets
+	$(".project").mouseenter(function(){
+		var project = "\#"+$(this).attr("id");
+		Snap.select(project).animate({fill: 'red'}, 100, mina.ease);
+		
+	});
+
+	$(".project").mouseleave(function(){
+		var project = "\#"+$(this).attr("id");
+		Snap.select(project).animate({fill: '#010102'}, 100, mina.ease);
+
+	});
+
+	$(".ligne").mouseover(function(){
+		var ligne = "\#"+$(this).attr("id")
+	});
+
+	$(".pole").mouseover(function(){
+		var ligne = "\#"+$(this).attr("id");
+	});
+
+
+
+
+
+
+	function test(){
+		console.log("je tesssssssssssssssst");
+	};
+
 
 });//$(document).ready END
 
@@ -25,39 +60,40 @@ $(window).load(function(){
 	var panZoomTiger = svgPanZoom('#map')
 
 	//Variables de positionnement de la carte
-	var svgPosX = Math.floor(($(window).width()/100)*40);
-	var svgPosY = Math.floor(($(window).height()/100)*55);
+	var svgPosX = Math.floor(($(window).width()/100)*46.6);
+	var svgPosY = Math.floor(($(window).height()/100)*53);
 
 	//Paramètres d'affichage de la carte
 	panZoomTiger.setMaxZoom(17);
 	panZoomTiger.setMinZoom(1);
-	panZoomTiger.zoomAtPointBy(4, {x: svgPosX, y: svgPosY});
+	panZoomTiger.zoomAtPointBy(11, {x: svgPosX, y: svgPosY});
 
 
-	//Interactions avec la map
-	/************************************************************/
+
+	
 
 	//Préparation pour pouvoir appeler les éléments de la carte dans
 	//le DOM SVG en jQuery sans confusions.
 	//var map = document.getElementById("map");
 	//console.log(map);
 
-	//hover sur un projet
-	$(".project").mouseover(function(){
-		var initialRadius = $(this).attr("r");
-		console.log(initialRadius);
-		$(this).attr("r","20");
-		return initialRadius;
-	});
-
-	$(".project").mouseleave(function(initialRadius){
-		console.log(initialRadius);
-		$(this).attr("r",initialRadius);
-	});
+	//var project = Snap(".project");
 
 	
 
 
+	/*
+	var seminaire = Snap("#seminaire");
+	var stuttgart = Snap("#stuttgart");
+	var transmedia = Snap("#transmedia");
+	var cinemalingua = Snap("#cinemalingua");
+	var stalker = Snap("#stalker");
+	var radiocomem = Snap("#radiocomem");
+	var paleo = Snap("#paleo");
+	*/
+
+
+	
 
 
 });//$(window).load END
